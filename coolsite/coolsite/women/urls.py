79001d7ca17131e0a656108e9fedcb500edb0493
marinesque <flamefiu@yandex.ru>
2023-10-05 13:07:05 +0200

    feat: add cats by num

---
+++ coolsite/coolsite/women/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index), #http://127.0.0.1:8000/women/, т.к. women/ определено в основном сщщдышеу urls
+    path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/women/cats/
+]
