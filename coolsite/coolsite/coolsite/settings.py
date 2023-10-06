de435ed6adf7c9dbd8cf7b1524433acb37c78472
marinesque <flamefiu@yandex.ru>
2023-10-06 11:35:32 +0200

    fix: add static files

--- coolsite/coolsite/coolsite/settings.py
+++ coolsite/coolsite/coolsite/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' #Будет добавляться ко всем url статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
